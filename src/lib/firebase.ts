50b712925da88067a68c34dfb2b1a2f1db6a13aa
adem <ademk8200@gmail.com>
2026-03-30 12:58:45 +0300

    fix(firebase): update api keys and add analytics

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,23 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZimL5svJEZok0H9S4Gub0x03z_X9F0_U",
+  apiKey: "AIzaSyCZiMl5svJEZokB3Hrf1Z9tT_tDKQYOZ5A",
   authDomain: "todo-d3dc5.firebaseapp.com",
   projectId: "todo-d3dc5",
-  storageBucket: "todo-d3dc5.appspot.com",
+  storageBucket: "todo-d3dc5.firebasestorage.app",
   messagingSenderId: "132578800821",
-  appId: "1:132578800821:web:d8bd6c8130833a693b79da"
+  appId: "1:132578800821:web:d5434cf9f0c09680c03afc",
+  measurementId: "G-Y7SM9TR15D"
 };
 
 // Initialize Firebase Application (safe for hot-reloads)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// Initialize Analytics
+export const analytics = getAnalytics(app);
 
 // Initialize Firebase Database (Firestore)
 export const db = getFirestore(app);
